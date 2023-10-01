cfd690668fa891006b9fc4f660e83608205bf4fb
yuppejp <yuppejp@gmail.com>
2023-10-01 09:45:31 +0900

    Initial Commit

---
+++ IconMaker/IconMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  IconMakerApp.swift
+//  IconMaker
+//
+//  Created by main on 2023/10/01.
+//
+
+import SwiftUI
+
+@main
+struct IconMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
