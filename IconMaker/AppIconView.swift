130934c6d4c963e953bfbdf7ebd1c8024ec6dcf3
yuppejp <yuppejp@gmail.com>
2023-10-02 19:54:11 +0900

    Initial Commit

---
+++ IconMaker/AppIconView.swift
@@ -0,0 +1,18 @@
+//
+//  AppIconView.swift
+//  IconMaker
+//
+//  Created by main on 2023/10/01.
+//
+
+import SwiftUI
+
+struct AppIconView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppIconView()
+}
