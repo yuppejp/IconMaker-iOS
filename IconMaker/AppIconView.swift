a9d2fc9ea6e8131e06971d42d661e49d3ab8a653
yuppejp <yuppejp@gmail.com>
2023-10-02 19:54:23 +0900

    Initial Commit

--- IconMaker/AppIconView.swift
+++ IconMaker/AppIconView.swift
@@ -8,11 +8,32 @@
 import SwiftUI
 
 struct AppIconView: View {
+    var systemName: String  // SF Symbolsの名前
+    var color: AnyGradient  // グラデーションカラー(iOS16〜)
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geometry in
+            ZStack {
+                Rectangle()
+                    .fill(color)
+                Image(systemName: systemName)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .foregroundColor(.white)
+                    .padding(geometry.size.width * 0.15) // 余白を多めにとる(全体の15%程度)
+            }
+            .offset(y: -2) // 上部の余白補正
+        }
     }
 }
 
 #Preview {
-    AppIconView()
+    VStack {
+        AppIconView(systemName: "square.and.pencil.circle", color: Color.red.gradient)
+            .frame(width: 150, height: 150)
+        AppIconView(systemName: "tram", color: Color.green.gradient)
+            .frame(width: 150, height: 150)
+        AppIconView(systemName: "sailboat.fill", color: Color.blue.gradient)
+            .frame(width: 150, height: 150)
+    }
 }
--- IconMaker/ContentView.swift
+++ IconMaker/ContentView.swift
@@ -6,19 +6,68 @@
 //
 
 import SwiftUI
+import Photos
 
 struct ContentView: View {
+    var iconName = "square.and.pencil.circle"
+    var iconColor = Color.red.gradient
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            AppIconView(systemName: iconName, color: iconColor)
+                .frame(width: 300, height: 300)
+            
+            Button("Save Image") {
+                saveToPhotos()
+            }
         }
-        .padding()
+    }
+    
+    private func saveToPhotos() {
+        DispatchQueue.main.async {
+            let viewSize = CGSize(width: 1024, height: 1024)
+            
+            let hostingController = UIHostingController(
+                rootView: AppIconView(systemName: iconName, color: iconColor)
+                    .frame(width: viewSize.width, height: viewSize.height))
+            
+            hostingController.view.frame = CGRect(origin: .zero, size: viewSize)
+            hostingController.view.backgroundColor = .clear
+            
+            let image = hostingController.snapshot(viewSize: viewSize)
+            
+            // PNGとしてカメラロールに保存
+            if let data = image.pngData() {
+                PHPhotoLibrary.shared().performChanges({
+                    let creationRequest = PHAssetCreationRequest.forAsset()
+                    creationRequest.addResource(with: .photo, data: data, options: nil)
+                }) { success, error in
+                    if let error = error {
+                        print("Error saving the image: \(error)")
+                    }
+                }
+            }
+        }
+    }
+}
+
+extension UIViewController {
+    func snapshot(viewSize: CGSize) -> UIImage {
+        let rendererFormat = UIGraphicsImageRendererFormat.default()
+        rendererFormat.scale = 1
+        let renderer = UIGraphicsImageRenderer(size: viewSize, format: rendererFormat)
+        
+        let image = renderer.image { _ in
+            let context = UIGraphicsGetCurrentContext()!
+            context.interpolationQuality = .high
+            view.drawHierarchy(in: CGRect(origin: .zero, size: viewSize), afterScreenUpdates: true)
+        }
+        
+        return image
     }
 }
 
 #Preview {
     ContentView()
 }
+
